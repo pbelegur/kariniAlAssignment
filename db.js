230a852cef94b389ea60ab64605d749b39553574
pbelegur <pbelegur@asu.edu>
2025-05-01 18:58:39 -0700

    Update db.js

--- db.js
+++ db.js
@@ -10,7 +10,7 @@ const client = new MongoClient(uri);
 let collection;
 let cartCollection;
 app.use(cors({
-  origin: 'http://localhost:3000'
+  origin: '*'
 }));
 app.use(express.json()); // 👈 MUST be before routes
 
